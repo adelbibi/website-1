f9f31d542b8745b133942aa5fc13db8310543f00
Humam Alwassel <haa54@cornell.edu>
2017-12-05 03:47:56 +0300

    added script for publishing

---
+++ publish_website.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if [[ $(git status -s) ]]
+then
+    echo "The working directory is dirty. Please commit any pending changes."
+    exit 1;
+fi
+
+echo "Pushing Changes to Website Repo"
+git push -u origin master
+
+echo "Publishing website"
+cd public
+git add .
+git commit -m "Build website (publish_website.sh)"
+git push -u origin master
+cd ..
